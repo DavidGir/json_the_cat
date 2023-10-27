3f8ab9fea9d7f626a2f8778971a4dd43794a62f6
David Giroux <DavidGiroux149@gmail.com>
2023-10-26 21:56:44 -0400

    made changes to breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,8 @@
 const request = require("request");
-const fs = require("fs");
 
-// Parse command-line arguments to get the URL and the file path:
+// Parse command-line arguments to get the URL:
 const url = process.argv[2];
-const filePath = process.argv[3];
+
 // If the input in command line is not a url or filepath; print a message to tell user proper usage input:
 if (!url || !filePath) {
   console.log("You should input in your terminal: node fetcher.js <url> <filePath>");
@@ -24,18 +23,16 @@ request(url, (error, response, body) => {
     console.log(body);
     console.log(typeof body);
   }
+  //Parsing string data into object with JSON:
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-  
-  // Once receive data, write to local file (i.e:fs.writefile):
-  // Callback error is a function that will be executed after the write operation is completed or if error occurs:
-  // fs.writeFile(filePath, body, (error) => {
-  //   if (error) {
-  //     console.log("An error occurred while writing the file:", error);
-  //     return;
-  //   }
-  //   // Print to confirm that file has been successfully written:
-  //   console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
-  // });
+  // console.log(data);
+  // console.log(typeof data);
+  // Accessing the first entry in the data array:
+  // if (Array.isArray(data)) {
+  //   const firstEntry = data[0].weight;
+  //   console.log("First entry:", firstEntry);
+  // //Just to return that its not an array if it is the case:
+  // } else {
+  //   console.log("Data is not an array");
+  }
 });
