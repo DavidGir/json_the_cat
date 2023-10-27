b94d47b0242fc6fafc3d816ccad3499bfec76bb6
David Giroux <DavidGiroux149@gmail.com>
2023-10-27 10:11:47 -0400

    Refactored breedFetcher code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,37 +1,30 @@
 const request = require("request");
 
-// Parse command-line arguments to get the URL:
-const breedName = process.argv[2];
-
-// If the input in command line is not a breed name; print a message to tell user proper usage input:
-if (!breedName) {
-  console.log("You should input in your terminal: node breedFetcher.js <breedName>");
-  return;
-}
-// Initialize url for the API endpoint:
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-// Use the request library to make an HTTP GET request to the given URL:
-request(url, (error, response, body) => {
-  // Edge-Case: Request failed -> If there is a typo in url the error message will show on the terminal:
-  if (error) {
-    console.log("An error occurred:", error);
-    return;
-  }
-  // CHeck if response status code request is successful:
-  if (response.statusCode === 200) {
-    // console.log(body);
-    // console.log(typeof body);
-    //Parsing string data into object with JSON:
-    const data = JSON.parse(body);
-    // console.log(data);
-    // Edge-Case: Breed not found -> If breed name is not found:
-    if (data.length === 0) {
-      console.log("Breed not found.");
+// Refactor code to move request logic in a function:
+const fetchBreedDescription = function(breedName, callback) {
+  // Initialize url for the API endpoint:
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  // Use the request library to make an HTTP GET request to the given URL:
+  request(url, (error, response, body) => {
+    // Edge-Case: Request failed -> If request failed, callback is called with the error message:
+    if (error) {
+      callback("An error occurred: ${error}", null);
       return;
     }
-    // Output breed description on terminal for user to see:
-    const breedDescription = data[0].description;
-    console.log(breedDescription);
-  }
-});
+    // CHeck if response status code request is successful:
+    if (response.statusCode === 200) {
+    //Parsing string data into object with JSON:
+      const data = JSON.parse(body);
+      // Edge-Case: Breed not found -> If breed is not found callback is called with a not found:
+      if (data.length === 0) {
+        callback("Breed not found.", null);
+        return;
+      }
+      // If no error and everything is ok, callback is called with null to signal no error:
+      const breedDescription = data[0].description;
+      callback(null, breedDescription);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
