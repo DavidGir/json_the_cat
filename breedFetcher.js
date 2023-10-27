d326a5b9b7f9fede958e17d2c7b9db3bf9d31e59
David Giroux <DavidGiroux149@gmail.com>
2023-10-26 22:07:20 -0400

    made further changes to the code to print output

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,15 @@
 const request = require("request");
 
 // Parse command-line arguments to get the URL:
-const url = process.argv[2];
+const breedName = process.argv[2];
 
-// If the input in command line is not a url or filepath; print a message to tell user proper usage input:
-if (!url || !filePath) {
-  console.log("You should input in your terminal: node fetcher.js <url> <filePath>");
+// If the input in command line is not a breed name; print a message to tell user proper usage input:
+if (!breedName) {
+  console.log("You should input in your terminal: node breedFetcher.js <breedName>");
   return;
 }
-// Get a visual print to see input info:
-console.log(`You provided the URL: ${url}`);
-console.log(`You provided the file path: ${filePath}`);
+// Initialize url for the API endpoint:
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 // Use the request library to make an HTTP GET request to the given URL:
 request(url, (error, response, body) => {
@@ -20,19 +19,13 @@ request(url, (error, response, body) => {
   }
   // CHeck if response status code request is successful:
   if (response.statusCode === 200) {
-    console.log(body);
-    console.log(typeof body);
-  }
-  //Parsing string data into object with JSON:
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  // Accessing the first entry in the data array:
-  // if (Array.isArray(data)) {
-  //   const firstEntry = data[0].weight;
-  //   console.log("First entry:", firstEntry);
-  // //Just to return that its not an array if it is the case:
-  // } else {
-  //   console.log("Data is not an array");
+    // console.log(body);
+    // console.log(typeof body);
+    //Parsing string data into object with JSON:
+    const data = JSON.parse(body);
+    // console.log(data);
+    // Output breed description on terminal for user to see:
+    const breedDescription = data[0].description;
+    console.log(breedDescription);
   }
 });
